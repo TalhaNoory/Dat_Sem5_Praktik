e858e0557b9f61af46fc769bfbbb55b8973c610d
Ahmad Talha Noory <talha.nuri@gmail.com>
2020-09-03 13:20:17 +0200

    Got my picture out, now i need to adjust something else

--- basic-react-router/src/Components/ItemDetail.js
+++ basic-react-router/src/Components/ItemDetail.js
@@ -4,29 +4,54 @@ import '../App.css';
 // State is holding the information that we getting from the API
 // Effect basically runs the fetch-call when our component mounts
 
-function ItemDetail({ match }) {
+function ItemDetail() {
 
     useEffect(() => {
         fetchItem();
-        console.log(match)
     }, []) //<-- This will only run, when the component mounts
 
-    const [item, setItem] = useState({});
+    const [item, setItem] = useState({
+        "types": [
+            {
+                "slot": "",
+                "type": {
+                    "name": "",
+                    "url": ""
+                }
+            },
+            {
+                "slot": "",
+                "type": {
+                    "name": "",
+                    "url": ""
+                }
+            }
+        ],
+        "sprites": {
+            "back_default":"",
+            "back_female":"",
+            "back_shiny":"",
+            "back_shiny_female":"",
+            "front_default":"",
+            "front_female":"",
+            "front_shiny":"",
+            "front_shiny_female":""
+        }
+    });
 
     const fetchItem = async () => {
         const fetchItem = await fetch(
-            `https://pokeapi.co/api/v2/pokemon?offset=
-            ${match.params.id}`
+            `https://pokeapi.co/api/v2/pokemon/1`
         )
 
         const item = await fetchItem.json();
         setItem(item)
-        // console.log(name)
+        // console.log(item)
     }
 
     return (
         <div>
-            <h1>Item</h1>
+            <img src={item.sprites.back_default} alt=''></img>
         </div>
     );
 }
