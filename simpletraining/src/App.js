03a694303c9e48ff5d7e6000051b22587cbae325
Ahmad Talha Noory <talha.nuri@gmail.com>
2020-09-07 12:54:04 +0200

    added new project 'raahandel-projekt'

--- simpletraining/src/App.js
+++ simpletraining/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import PokemonForm from './SimpleList'
+// import PokemonForm from './SimpleList'
 // import NameForm from './PrintOneValue'
 // import NameImageForm from './NameWithImage'
+import CreateId from './CreatingID'
 
 function App() {
   return (
-    <PokemonForm /> 
+    // <PokemonForm /> 
     // <NameForm />
     // <NameImageForm />
+    <CreateId />
   );
 }
 
